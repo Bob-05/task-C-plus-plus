bb3c6c051b050b6bf3d54d1827fa4aae5fb229c3
Azizov Bahtiar <166922623+Bob-05@users.noreply.github.com>
2025-03-24 23:09:41 +0300

    data types (типы данных)

---
+++ data types (типы данных)/PrintText.cpp
@@ -0,0 +1,30 @@
+/*
+Печать текста
+Ограничение времени: 1 с
+Ограничение памяти: 64.0 Мб
+Ввод: стандартный ввод или input.txt
+Вывод: стандартный вывод или output.txt
+
+
+Напишите программу, печатающую на экране первые строчки со страницы Бьярне Страуструпа про C++:
+
+C++ is a general-purpose programming language with a bias towards systems programming that
+  - is a better C
+  - supports data abstraction
+  - supports object-oriented programming
+  - supports generic programming.
+Не потеряйте парные пробелы в начале строк и переносы в конце строк.
+
+*/
+
+#include <iostream> 
+using namespace std;
+
+int main() {
+cout << "C++ is a general-purpose programming language with a bias towards systems programming that\n";
+cout << "  - is a better C\n";
+cout << "  - supports data abstraction\n";
+cout << "  - supports object-oriented programming\n";
+cout << "  - supports generic programming.\n";
+
+}
